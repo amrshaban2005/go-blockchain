4c734dc658b00f3d7fc592fca07ea83d6d99af96
amrshaban2005 <amr_shaban2005@yahoo.com>
2024-10-18 00:11:51 +0800

    sync blockchain nodes

--- blockchain_node/blockchain_node.go
+++ blockchain_node/blockchain_node.go
@@ -95,8 +95,42 @@ func (bcn *BlockchainNode) Transactions(w http.ResponseWriter, r *http.Request) 
 		}
 
 		io.WriteString(w, string(m))
-		return
 
+	case http.MethodPut:
+		decoder := json.NewDecoder(r.Body)
+		var t blockchain.TransactionRequest
+		err := decoder.Decode(&t)
+		if err != nil {
+			log.Printf("Error: %v", err)
+			io.WriteString(w, string(utils.JsonStatus("fail")))
+			return
+		}
+
+		if !t.Validate() {
+			log.Println("Error: missing filed(s)")
+			io.WriteString(w, string(utils.JsonStatus("fail")))
+			return
+		}
+		publicKey := utils.String2PublicKey(*t.SenderPublicKey)
+		signature := utils.String2Signature(*t.Signature)
+		bc := bcn.GetBlockchain()
+		isUpdated := bc.AddTransactions(*t.SenderBlockchainAddress, *t.RecipientBlockchainAddress, *t.Value, publicKey, signature)
+		w.Header().Add("Content-Type", "application/json")
+		var m []byte
+		if !isUpdated {
+			w.WriteHeader(http.StatusBadRequest)
+			m = utils.JsonStatus("fail")
+		} else {
+
+			m = utils.JsonStatus("success")
+		}
+
+		io.WriteString(w, string(m))
+	case http.MethodDelete:
+		bc := bcn.GetBlockchain()
+		bc.ClearTransactionPool()
+
+		io.Writer.Write(w, utils.JsonStatus("success"))
 	default:
 		log.Printf("Error: invalid http method")
 		w.WriteHeader(http.StatusBadRequest)
@@ -140,11 +174,56 @@ func (bcn *BlockchainNode) StartMine(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func (bcn *BlockchainNode) Amount(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		blockchainAddress := r.URL.Query().Get("blockchain_address")
+		amount := bcn.GetBlockchain().CalculateTotalAmount(blockchainAddress)
+
+		ar := &blockchain.AmountResponse{Amount: amount}
+		m, _ := ar.MarshalJSON()
+
+		w.Header().Add("Content-Type", "application/json")
+		io.WriteString(w, string(m[:]))
+
+	default:
+		log.Println("Error: invalid http method")
+		io.Writer.Write(w, utils.JsonStatus("fail"))
+	}
+}
+
+func (bcn *BlockchainNode) Consensus(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodPut:
+		bc := bcn.GetBlockchain()
+		replaced := bc.ResolveConflicts()
+		w.Header().Add("Content-Type", "application/json")
+		var m []byte
+		if !replaced {
+			m = utils.JsonStatus("fail")
+		} else {
+			m = utils.JsonStatus("success")
+		}
+
+		io.Writer.Write(w, utils.JsonStatus(string(m)))
+
+	default:
+		log.Println("Error: invalid http method")
+		io.Writer.Write(w, utils.JsonStatus("fail"))
+	}
+
+}
+
 func (bcn *BlockchainNode) Run() {
+	bc := bcn.GetBlockchain()
+	bc.Run()
+
 	http.HandleFunc("/", bcn.GetChain)
 	http.HandleFunc("/transactions", bcn.Transactions)
 	http.HandleFunc("/mine", bcn.Mine)
 	http.HandleFunc("/mine/start", bcn.StartMine)
+	http.HandleFunc("/amount", bcn.Amount)
+	http.HandleFunc("/consensus", bcn.Consensus)
 
 	log.Fatal(http.ListenAndServe("0.0.0.0:"+strconv.Itoa(int(bcn.Port())), nil))
 }
